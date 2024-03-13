96dad137dbd86f043ed6be340cc7ccb272d82833
wenbo.yang <wenbo.yang.84@gmail.com>
2024-03-12 22:57:26 -0700

    merge

--- src/utils/imageConverter/imageConverter.ts
+++ src/utils/imageConverter/imageConverter.ts
@@ -1,6 +1,7 @@
 import Jimp from 'jimp';
 import { BitMapBuffer } from '../bitMapBuffer';
 import { Config } from '../../config';
+import { decode, encode } from 'bmp-js';
 
 export class ImageConverter {
     private config: Config;
@@ -11,16 +12,31 @@ export class ImageConverter {
 
     public async convertAndResizeToBMP(buffer: Buffer): Promise<BitMapBuffer> {
         const jimp = await Jimp.read(Buffer.from(buffer));
-        const ratio = jimp.getHeight() / jimp.getWidth();
+        const bmpImage = decode(await jimp.grayscale().getBufferAsync(Jimp.MIME_BMP));
 
-        if (ratio > 1) {
-            jimp.resize(Math.floor(this.config.imageWidth / ratio), this.config.imageHeight);
-        } else {
-            jimp.resize(this.config.imageWidth, Math.floor(this.config.imageHeight * ratio));
+        // get the box
+        let top = Number.MAX_VALUE;
+        let bottom = Number.MIN_VALUE;
+        let right = Number.MIN_VALUE;
+        let left = Number.MAX_VALUE;
+        let index = 0;
+
+        for (let i = 0; i < bmpImage.height; i++) {
+            for (let j = 0; j < bmpImage.width; j++) {
+                if ((bmpImage.data[index + 1] + bmpImage.data[index + 2] + bmpImage.data[index + 3]) / 3 <= this.config.grayScaleWhiteThreshold) {
+                    top = Math.min(i, top);
+                    bottom = Math.max(i, bottom);
+                    left = Math.min(j, left);
+                    right = Math.max(j, right);
+                }
+
+                index += 4;
+            }
         }
 
-        const data = await jimp.grayscale().getBufferAsync(Jimp.MIME_BMP);
+        jimp.crop(left, top, right - left, bottom - top).resize(this.config.imageWidth, this.config.imageHeight);
 
+        const data = await jimp.grayscale().getBufferAsync(Jimp.MIME_BMP);
         return new BitMapBuffer(data, this.config.imageHeight, this.config.imageWidth);
     }
 }
